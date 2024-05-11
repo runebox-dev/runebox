57d8f6a922b62a07f08b0a058a2913dd6fea8af2
Layto <122397026+laytodev@users.noreply.github.com>
2024-05-11 05:32:33 -0400

    Remove jar archive dependencies from ClassGroup tests
    The dependencies on JarArchive and associated File were removed from the ClassGroupTests class. These changes simplify the test scenarios and prevent the need to write archives, focusing the test case purely on verifying the presence of classes in the group.

--- internal/runebox.asm/src/test/kotlin/io/runebox/asm/ClassGroupTests.kt
+++ internal/runebox.asm/src/test/kotlin/io/runebox/asm/ClassGroupTests.kt
@@ -1,13 +1,10 @@
 package io.runebox.asm
 
 import io.kotest.matchers.ints.shouldBeGreaterThan
-import io.runebox.asm.archive.JarArchive
-import java.io.File
 
 class ClassGroupTests : AsmSpec({
 
     test("Load classes from jar.") {
         group.allClasses.size shouldBeGreaterThan 0
-        group.writeArchive(JarArchive(File("../../gamepack.out.jar")))
     }
 })
